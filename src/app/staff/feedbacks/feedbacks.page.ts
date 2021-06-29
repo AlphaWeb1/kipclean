8f21899646abb878d89c2a6a6a9643a988797432
Abdulwasiu Majoyeogbe <abdulwasihitopee4real@gmail.com>
2021-06-29 19:00:52 +0100

    update on changes

--- src/app/staff/feedbacks/feedbacks.page.ts
+++ src/app/staff/feedbacks/feedbacks.page.ts
@@ -76,6 +76,8 @@ export class FeedbacksPage implements OnInit {
       this.feedbacks = [];
         if (res?.data) {
           const feedbacks = res.data;
+          console.log(feedbacks);
+          
           feedbacks.forEach(feedback => feedback.date_string = dayjs(feedback.createdAt).format('ddd, D MMM YYYY') );
           this.feedbacks = feedbacks;
           this.isLoading = false;
